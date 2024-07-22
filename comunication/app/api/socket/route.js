c57e29c6a9590dd4c3aefef93c2bf8d2e918ee3c
muhfaiqthirafi46 <muhfaiqthirafi46@gmail.com>
2024-07-22 10:32:17 +0000

    updt

--- comunication/app/api/socket/route.js
+++ comunication/app/api/socket/route.js
@@ -1,17 +1,21 @@
-import { Server } from 'ws'
+import { WebSocketServer } from 'ws';
 
-const wss = new Server({ noServer: true })
+let wss;
 
-wss.on('connection', socket => {
-  socket.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  socket.send('Hello! Message From Server!!')
-})
+if (!global.wss) {
+  wss = new WebSocketServer({ noServer: true });
+  global.wss = wss;
+
+  wss.on('connection', (socket) => {
+    socket.on('message', (message) => {
+      console.log(`Received message => ${message}`);
+    });
+    socket.send('Hello! Message From Server!!');
+  });
+} else {
+  wss = global.wss;
+}
 
 export async function GET(request) {
-  if (!global.wss) {
-    global.wss = wss
-  }
-  return new Response(null, { status: 200 })
+  return new Response(null, { status: 200 });
 }
