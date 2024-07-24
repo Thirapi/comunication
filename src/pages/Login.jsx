70c8043adaf491913e4209b8393675edd7b1c50c
muhfaiqthirafi46 <muhfaiqthirafi46@gmail.com>
2024-07-24 12:04:58 +0000

    Add Bcrypt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../supabaseClient';
+import bcrypt from 'bcryptjs';
 
 const Login = () => {
   const [username, setUsername] = useState('');
