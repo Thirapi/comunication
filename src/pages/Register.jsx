f34d4adbd1568e449b5e0d80ef15d032305af596
muhfaiqthirafi46 <muhfaiqthirafi46@gmail.com>
2024-07-24 01:12:33 +0000

    Initial commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
     if (error) {
       console.error('Error registering user', error);
     } else {
-      navigate('/login'); // Redirect to login after successful registration
+      navigate('/'); // Redirect to login after successful registration
     }
   };
 
