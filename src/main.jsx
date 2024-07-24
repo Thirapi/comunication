89b929dba6f766cd19467d8bcfba02c633ad10de
muhfaiqthirafi46 <muhfaiqthirafi46@gmail.com>
2024-07-24 00:52:44 +0000

    Kembali kefitrah

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Chat from './pages/Chat';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
