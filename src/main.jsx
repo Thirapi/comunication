6dee9ac2edf757944649ada5d9a20bcb98bc4ed1
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-08-10 11:59:02 +0700

    update routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/reg" element={<Register />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
