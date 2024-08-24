648886e852f28cb1d71a7de1b737c30f4b132a3e
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-08-24 06:33:52 +0000

    update

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -83,12 +83,12 @@ const Chat = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center bg-slate-900"
-    style={{
-      backgroundImage: `url(${image})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      backgroundAttachment: 'fixed'
-    }}>
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundAttachment: 'fixed'
+      }}>
       <div className="bg-slate-800 text-white rounded shadow-md p-8 w-full max-w-lg my-8 h-[calc(100vh-4rem)] border-solid border-slate-300 border-2 shadow-lg rounded-md flex flex-col">
         <div className="flex justify-between items-center mb-6 text-white">
           <h1 className="text-2xl font-bold">Komunikasi</h1>
@@ -99,50 +99,36 @@ const Chat = () => {
             Logout
           </button>
         </div>
-        <div className="overflow-y-auto flex-1 mb-4">
-          {/* {messages.map((msg) => (
-            <div key={msg.id} className="mb-2">
-              <strong>{msg.username}</strong>: {msg.message}
-            </div>
-          ))} */}
-          {messages.map((msg) => (
-            <div key={msg.id} className="mb-4 p-2 rounded transition-colors duration-200 hover:bg-slate-900">
-              <div className="flex items-center mb-1">
-                <strong className="mr-2">{msg.username}</strong>
-                <span className="text-gray-500 text-sm">{dayjs(msg.created_at).format('DD/MM/YYYY h:mm A')}</span>
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="overflow-y-auto flex-1 mb-4">
+            {messages.map((msg) => (
+              <div key={msg.id} className="mb-4 p-2 rounded transition-colors duration-200 hover:bg-slate-900">
+                <div className="flex items-center mb-1">
+                  <strong className="mr-2">{msg.username}</strong>
+                  <span className="text-gray-500 text-sm">{dayjs(msg.created_at).format('DD/MM/YYYY h:mm A')}</span>
+                </div>
+                <div>{msg.message}</div>
               </div>
-              <div>{msg.message}</div>
-            </div>
-          ))}
-          <div ref={messageEndRef} />
+            ))}
+            <div ref={messageEndRef} />
+          </div>
+          <form onSubmit={handleSubmit} className="w-full flex items-center p-2 bg-slate-700">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="flex-grow px-4 py-2 text-black border rounded-full mr-2"
+              placeholder="Type your message..."
+              required
+            />
+            <button type="submit" className="bg-blue-500 text-white p-3 rounded-full">
+              <FaPaperPlane />
+            </button>
+          </form>
         </div>
-        {/* <form onSubmit={handleSubmit} className="w-full">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full px-4 py-2 text-black border rounded-md mb-2"
-            placeholder="Type your message..."
-            required
-          />
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md">➣</button>
-        </form> */}
-        <form onSubmit={handleSubmit} className="w-full flex items-center">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="flex-grow px-4 py-2 text-black border rounded-full mb-2 mr-2"
-            placeholder="Type your message..."
-            required
-          />
-          <button type="submit" className="bg-blue-500 text-white p-3 rounded-full mb-2">
-            <FaPaperPlane />
-          </button>
-        </form>
       </div>
     </div>
-  );
+  );  
 };
 
 export default Chat;
